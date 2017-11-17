b6a2d18062ae28691158a66242046eeb7028b646
Wojciech Zwiefka <wojciech.zwiefka@getresponse.com>
2017-11-17 10:32:05 +0100

    check limit in test

--- limitedcache_test.go
+++ limitedcache_test.go
@@ -2,19 +2,22 @@ package limitedcache
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
 func TestDiskCache(t *testing.T) {
+	cacheLimit := 10
 	tempDir, err := ioutil.TempDir("", "httpcache")
 	if err != nil {
 		t.Fatalf("TempDir: %v", err)
 	}
 	defer os.RemoveAll(tempDir)
 
-	cache := New(tempDir, 10)
+	cache := New(tempDir, cacheLimit)
 
 	key := "testKey"
 	_, ok := cache.Get(key)
@@ -39,4 +42,31 @@ func TestDiskCache(t *testing.T) {
 	if ok {
 		t.Fatal("deleted key still present")
 	}
+
+	val = []byte("some bytes")
+	for i := 0; i < 2*cacheLimit; i++ {
+		cache.Set(fmt.Sprintf("some key:%d", i), val)
+	}
+
+	count, err := countFiles(tempDir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if count != cacheLimit {
+		t.Fatalf("expected %d files, got: %d", cacheLimit, count)
+	}
+}
+
+func countFiles(basePath string) (int, error) {
+	var count int
+	err := filepath.Walk(basePath, func(path string, f os.FileInfo, err error) error {
+		if err == nil && !f.IsDir() {
+			count++
+		}
+		return err
+	})
+	if err != nil {
+		return 0, err
+	}
+	return count, nil
 }
